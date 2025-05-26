9adc59eef13cdc35d4c22ee143708873811bb1b3
alan vasquez loaeza <alanvasloadev@gmail.com>
2025-05-26 11:53:20 -0600

    pages and full template change language

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <HomeIntro />
-        <h1 class="text-3xl font-bold underline">{{ $t('welcome') }}</h1>
+        <HomeSocialLinks />
+        <HomeFeaturedProjects />
+        <HomeFeaturedArticles />
+        <HomeNewsletter />
     </div>
 </template>
