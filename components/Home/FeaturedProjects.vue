f1a1f43d9e1ad8db8523cfeb2942b6a790603a13
alan vasquez loaeza <alanvasloadev@gmail.com>
2025-11-19 10:19:37 -0600

    translate the projects

--- components/Home/FeaturedProjects.vue
+++ components/Home/FeaturedProjects.vue
@@ -4,9 +4,16 @@
       {{ $t('projects.title') }}
     </h2>
     <div class="space-y-4">
-      <AppProjectCard
+     <AppProjectCard
+        v-if="locale === 'es'"
         v-for="(project, id) in projects"
         :key="id"
+        :project="project"
+      />
+      <AppProjectCard
+        v-else
+        v-for="(project, id) in projesctsEn"
+        :key="'en'+id"
         :project="project"
       />
     </div>
@@ -26,4 +33,57 @@ import projects from "~/assets/data/projects.json";
 //const { data: projects } = await useAsyncData("projects-home", () =>
  // queryContent("/projects").limit(3).find()
 //);
+const { t , locale, locales} = useI18n() 
+
+const projesctsEn  = [
+  {
+  "name": "FINANZAS DE OAXACA (SEFIN)",
+  "url": "https://www.finanzasoaxaca.gob.mx/",
+  "description": "In this project, I collaborated with the SEFIN team to develop a financial management system for the government of the state of Oaxaca.",
+  "thumbnail": "/projects/cropped-cropped-log-sefin.png",
+  "tecnhologies": "SpringBoot, Java, PostgresQL, Bootstrap",
+  "date": "2017 - 2019",
+  "status": "WIP",
+  "opensource": false
+},
+{
+  "name": "Isbast.cl",
+  "url": "https://isbast.com/",
+  "description": "Isbast is a company in the real estate sector.",
+  "thumbnail": "/projects/isbast.png",
+  "tecnhologies": "Laravel, Vuejs, MySQL, Bootstrap",
+  "date": "2019 - 2021",
+  "status": "Active"
+},
+{
+  "name": "usplat",
+  "url": "https://usplat.com/",
+  "description": "A simple white-noise app that stays in the menu bar.",
+  "thumbnail": "/projects/usplat.png",
+  "tecnhologies": "Electron, Vuejs, Nodejs, TailwindCSS, Laravel",
+  "status": "Active",
+  "date": "2021 - 2021",
+  "opensource": true
+},
+{
+  "name": "Dropstudio",
+  "url": "https://github.com/fayazara/pocketbase-nuxt",
+  "description": "Development and maintenance of web applications using Laravel and related technologies such as Laravel and Vue.js.",
+  "thumbnail": "/projects/drop.png",
+  "status": "Active",
+  "tecnhologies": "Vuejs, TailwindCSS, Laravel, AWS S3",
+  "date": "2013 - 2019",
+  "opensource": true
+},
+{
+  "name": "CVA",
+  "url": "https://www.grupocva.com",
+  "description": "Backend web developer creating web applications and RESTful APIs using Laravel and related technologies. Collaborated on agile development projects, participating in daily meetings and code reviews.",
+  "thumbnail": "/projects/cva.png",
+  "tecnhologies": "Php, Laravel, OracleDb, TailwindCSS",
+  "status": "WIP",
+  "date": "2017 - 2019",
+  "opensource": true
+}
+]
 </script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,10 +1,18 @@
 <template>
   <main class="min-h-screen">
-    <AppHeader class="mb-12" title="Projects" :description="description" />
+    <AppHeader class="mb-12" :title="$t('projects.description')" :description="$t('projects.description')" />
     <div class="space-y-4">
+      
       <AppProjectCard
+        v-if="locale === 'es'"
         v-for="(project, id) in projects"
         :key="id"
+        :project="project"
+      />
+      <AppProjectCard
+        v-else
+        v-for="(project, id) in projesctsEn"
+        :key="'en'+id"
         :project="project"
       />
     </div>
@@ -13,15 +21,65 @@
 
 <script setup>
 import projects from "~/assets/data/projects.json";
+const { t , locale, locales} = useI18n()
 
-const description =
-  "I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved.";
 useSeoMeta({
   title: "Projects | Alan Vasquez",
-  description,
+  description :t('projects.description'),
 });
 
 //const { data: projects } = await useAsyncData("projects-all", () =>
  // queryContent("/projects").find()
 //);
+const projesctsEn  = [
+  {
+  "name": "FINANZAS DE OAXACA (SEFIN)",
+  "url": "https://www.finanzasoaxaca.gob.mx/",
+  "description": "In this project, I collaborated with the SEFIN team to develop a financial management system for the government of the state of Oaxaca.",
+  "thumbnail": "/projects/cropped-cropped-log-sefin.png",
+  "tecnhologies": "SpringBoot, Java, PostgresQL, Bootstrap",
+  "date": "2017 - 2019",
+  "status": "WIP",
+  "opensource": false
+},
+{
+  "name": "Isbast.cl",
+  "url": "https://isbast.com/",
+  "description": "Isbast is a company in the real estate sector.",
+  "thumbnail": "/projects/isbast.png",
+  "tecnhologies": "Laravel, Vuejs, MySQL, Bootstrap",
+  "date": "2019 - 2021",
+  "status": "Active"
+},
+{
+  "name": "usplat",
+  "url": "https://usplat.com/",
+  "description": "A simple white-noise app that stays in the menu bar.",
+  "thumbnail": "/projects/usplat.png",
+  "tecnhologies": "Electron, Vuejs, Nodejs, TailwindCSS, Laravel",
+  "status": "Active",
+  "date": "2021 - 2021",
+  "opensource": true
+},
+{
+  "name": "Dropstudio",
+  "url": "https://github.com/fayazara/pocketbase-nuxt",
+  "description": "Development and maintenance of web applications using Laravel and related technologies such as Laravel and Vue.js.",
+  "thumbnail": "/projects/drop.png",
+  "status": "Active",
+  "tecnhologies": "Vuejs, TailwindCSS, Laravel, AWS S3",
+  "date": "2013 - 2019",
+  "opensource": true
+},
+{
+  "name": "CVA",
+  "url": "https://www.grupocva.com",
+  "description": "Backend web developer creating web applications and RESTful APIs using Laravel and related technologies. Collaborated on agile development projects, participating in daily meetings and code reviews.",
+  "thumbnail": "/projects/cva.png",
+  "tecnhologies": "Php, Laravel, OracleDb, TailwindCSS",
+  "status": "WIP",
+  "date": "2017 - 2019",
+  "opensource": true
+}
+]
 </script>
